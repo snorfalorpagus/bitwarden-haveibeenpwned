9a5bd8bad6af8e37af1347adc077a8714ac5ae0a
Joshua Arnott <josh@snorfalorpagus.net>
2019-01-20 13:25:21 +0000

    Improved code readability

--- pwned.py
+++ pwned.py
@@ -3,44 +3,48 @@ import json
 import hashlib
 import re
 import requests
+from typing import Dict, Any
+import sys
 
 
-def get_hash(password):
+def get_hash(password: str) -> str:
     m = hashlib.sha1()
     m.update(password)
     password_hash = m.hexdigest().upper()
     return password_hash
 
 
-def get_pwned(key):
-    assert len(key) == 5
+def get_pwned(password_hash: str) -> Dict[str, int]:
+    key = password_hash[0:5]
     res = requests.get(f"https://api.pwnedpasswords.com/range/{key}")
     assert res.status_code == 200
-    results = {key+l[0]: int(l[1]) for l in [line.split(":") for line in res.text.split("\r\n")]}
+    lines = res.text.splitlines()
+    pairs = [line.split(":") for line in lines]
+    results = {f"{key}{ending}": int(count) for ending, count in pairs}
     return results
 
 
-def get_hostname(uri):
-    try:
-        hostname = re.search("://([^/]*)/", uri).group(1)
-    except AttributeError:
-        hostname = uri
-    return hostname
+def get_credentials() -> Dict[str, Any]:
+    result = subprocess.run(["bw", "list", "items"], capture_output=True)
+    items = json.loads(result.stdout)
+    return [item for item in items if "login" in item]
 
 
-result = subprocess.run(["bw", "list", "items"], capture_output=True)
-items = json.loads(result.stdout)
+def main():
+    count_ok = 0
+    count_pwned = 0
+    for item in get_credentials():
+        password = item["login"]["password"].encode("utf-8")
+        password_hash = get_hash(password)
+        results = get_pwned(password_hash)
+        pwned = password_hash in results
+        if not pwned:
+            count_ok += 1
+        else:
+            count_pwned += 1
+            print(f"{item['name']} has been pwned!")
 
-for item in items:
-    if not "login" in item:
-        continue
-    password = item["login"]["password"].encode("utf-8")
-    password_hash = get_hash(password)
-    results = get_pwned(password_hash[0:5])
-    pwned = password_hash in results
-    if not pwned:
-        continue
-    hostname = get_hostname(item["login"]["uris"][0]["uri"])
-    print(f"{hostname} HAS BEEN PWNED!!!")
-    
+    print(f"{count_pwned} of {count_ok} logins have been pwned.")
 
+if __name__ == "__main__":
+    sys.exit(main())
